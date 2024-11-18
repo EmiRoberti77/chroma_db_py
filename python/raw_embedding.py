28dddf188f06ab0188346a77f8c17a24eaa90c12
EmiRoberti77 <emi@datamotion.ai>
2024-11-18 17:43:37 +0000

    openai embedding

---
+++ python/raw_embedding.py
@@ -0,0 +1,13 @@
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+load_dotenv()
+client = OpenAI(
+    api_key=os.getenv('OPENAI_API_KEY')
+)
+
+response = client.embeddings.create(
+    input="this is my code and this line will be embedded", 
+    model="text-embedding-3-small")
+
+print(response)
